f5880c1f44c381c4ef3bbc05e73c9e98104a5e09
lhmuana <edd_vai@yahoo.co.in>
2023-10-03 14:43:49 +0530

    The initial app

--- app/src/main/java/com/example/material_theming_app/ui/theme/Theme.kt
+++ app/src/main/java/com/example/material_theming_app/ui/theme/Theme.kt
@@ -25,23 +25,13 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
 @Composable
 fun Material_Theming_AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -65,6 +55,7 @@ fun Material_Theming_AppTheme(
     MaterialTheme(
         colorScheme = colorScheme,
         typography = Typography,
+        shapes = Shapes,
         content = content
     )
 }
