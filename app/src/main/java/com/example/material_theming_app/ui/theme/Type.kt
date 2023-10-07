ee5c0c6cc01e6423f6796cf552e82a0df889e799
lhmuana <edd_vai@yahoo.co.in>
2023-10-07 20:44:23 +0530

    typography complted

--- app/src/main/java/com/example/material_theming_app/ui/theme/Type.kt
+++ app/src/main/java/com/example/material_theming_app/ui/theme/Type.kt
@@ -2,20 +2,52 @@ package com.example.material_theming_app.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.material_theming_app.R
+
+
+val AbrilFatFace = FontFamily(
+    Font(R.font.abril_fatface_regular)
+)
+
+val Montserrat = FontFamily(
+    Font(R.font.montserrat_regular),
+    Font(R.font.montserrat_bold, FontWeight.Bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Montserrat,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 14.sp,
+
+    ),
+
+    displayLarge = TextStyle(
+            fontFamily = AbrilFatFace,
+            fontWeight = FontWeight.Normal,
+            fontSize = 36.sp
+            ),
+
+    displayMedium = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+
+    labelSmall = TextStyle(
+        fontFamily = Montserrat,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp
     )
-    /* Other default text styles to override
+
+
+
+/* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Normal,
